7e25e4ef6646abf265d8d4797f9a1db72877c170
daiki578 <satodai0825@icloud.com>
2025-04-21 14:28:13 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const mario = document.getElementById("mario");
 const obstacle = document.getElementById("obstacle");
 const gameOverText = document.getElementById("game-over");
+const startButton = document.getElementById("start-button");
 
-// ジャンプ操作
+let gameRunning = false;
+let collisionCheck;
+
 document.addEventListener("keydown", () => {
-  if (!mario.classList.contains("jump")) {
+  if (gameRunning && !mario.classList.contains("jump")) {
     mario.classList.add("jump");
 
     setTimeout(() => {
@@ -13,16 +16,24 @@ document.addEventListener("keydown", () => {
   }
 });
 
-// 衝突判定（ゲーム開始後500msから判定スタート）
-setTimeout(() => {
-  let checkCollision = setInterval(() => {
+startButton.addEventListener("click", () => {
+  // 初期化
+  obstacle.classList.add("move");
+  gameOverText.classList.add("hidden");
+  startButton.classList.add("hidden");
+  gameRunning = true;
+
+  // 衝突判定開始
+  collisionCheck = setInterval(() => {
     const marioBottom = parseInt(window.getComputedStyle(mario).getPropertyValue("bottom"));
     const obstacleRight = parseInt(window.getComputedStyle(obstacle).getPropertyValue("right"));
 
     if (obstacleRight > 550 && obstacleRight < 590 && marioBottom < 40) {
-      obstacle.style.animation = "none";
+      obstacle.classList.remove("move");
       gameOverText.classList.remove("hidden");
-      clearInterval(checkCollision);
+      startButton.classList.remove("hidden");
+      clearInterval(collisionCheck);
+      gameRunning = false;
     }
   }, 10);
-}, 500);
+});
