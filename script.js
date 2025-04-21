ca429a2b2518d9bfb77fc1edec8b571cc9121559
daiki578 <satodai0825@icloud.com>
2025-04-21 13:57:16 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-function sayHi() {
-  const greetings = [
-    "はじめまして！daikiです👋",
-    "みんなも沢山アニメを見よう！",
-    "今日も１日頑張ろう！",
-    "Hello from daiki 🚀"
-  ];
-  const index = Math.floor(Math.random() * greetings.length);
-  document.getElementById("message").textContent = greetings[index];
+function showMessage() {
+  document.getElementById("message").textContent = "来てくれてありがとう！😊";
 }
